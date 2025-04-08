c811a868825468d9936de45066bc9d12e6898331
ramoraga <70076287+ramoraga@users.noreply.github.com>
2025-04-08 16:33:59 -0500

    Update SYNGEN-N.py
    clean code

--- SYNGEN Scripts/SYNGEN-N.py
+++ SYNGEN Scripts/SYNGEN-N.py
@@ -8,7 +8,7 @@ def delete_existing_stl_models():
     for obj in bpy.context.scene.objects:
         if obj.type == 'MESH' and obj.name.startswith("Imported_"):
             bpy.data.objects.remove(obj, do_unlink=True)
-    # Clean orphan data
+
     for block in bpy.data.meshes:
         if block.users == 0:
             bpy.data.meshes.remove(block)
@@ -25,7 +25,6 @@ def reset_object_and_animation(obj, drop_location):
 
 delete_existing_stl_models()
 
-# Table dimensions
 table_height = 0.6 
 
 # Ensure the 'Table' object exists in the scene
@@ -38,27 +37,19 @@ empty_object = bpy.data.objects.get('Empty')
 if empty_object is None:
     raise ValueError("Object 'Empty' not found in the scene.")
 
-# Set the drop location above the table
-drop_location = (0, 0, table_height + 0.6)
-
 # Set physics properties for the table
 bpy.context.view_layer.objects.active = table_object
 bpy.ops.rigidbody.object_add()
 table_object.rigid_body.type = 'PASSIVE'
 table_object.rigid_body.friction = 1
 
-# Set the frame number when the animation should stop
 stop_frame = 25
-
-# Rotation angles for rendering
 z_angles = [0, 45, 90, 135, 180, 225, 270, 315]
 x_angles = [40, 30, 0, -30, -60, -90]
 
-# Rendering process
-for iteration in range(1, 6):
+for iteration in range(1, 2):
     print(f"\n=== Starting iteration {iteration} ===")
 
-    # Ensure scene updates
     bpy.context.view_layer.update()
 
     for x_index, x_angle in enumerate(x_angles, start=1):
@@ -72,8 +63,8 @@ for iteration in range(1, 6):
             bpy.context.scene.frame_set(stop_frame)
 
             # Specify output path
-            main_output_path = f"C:/Users/dinob/Desktop/CAD Project/Prismatic Geometries/null_{iteration}{x_index}{z_index}"
-            mask_output_path = f"null_m{iteration}{x_index}{z_index}"
+            main_output_path = f"C:/Users/dinob/Desktop/CAD Project/Prismatic Geometries/null_{iteration+5}{x_index}{z_index}"
+            mask_output_path = f"null_m{iteration+5}{x_index}{z_index}"
 
             # Set paths for each output node
             node_tree = bpy.context.scene.node_tree
