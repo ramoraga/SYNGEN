ee6aa51eaf267e2d03844182bf6741ca65d71a9b
ramoraga <70076287+ramoraga@users.noreply.github.com>
2025-04-08 16:32:32 -0500

    Update SYNGEN-O.py
    cleaned code

--- SYNGEN Scripts/SYNGEN-O.py
+++ SYNGEN Scripts/SYNGEN-O.py
@@ -9,15 +9,13 @@ def delete_existing_stl_models():
     for obj in bpy.context.scene.objects:
         if obj.type == 'MESH' and obj.name.startswith("Imported_"):
             bpy.data.objects.remove(obj, do_unlink=True)
-    # Clean orphan data
+
     for block in bpy.data.meshes:
         if block.users == 0:
             bpy.data.meshes.remove(block)
 
-
 # Function to reset the object's position, rotation, and animation
 def reset_object_and_animation(imported_object, drop_location):
-    # Reset the object's position and random rotation for a new drop
     imported_object.location = drop_location
     imported_object.rotation_euler = (
         random.uniform(0, 2 * 3.14159),  # Random rotation around the X-axis
@@ -34,7 +32,6 @@ def redefine_origin(imported_object):
     bpy.context.view_layer.objects.active = imported_object
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
 
-# Set the path to folder containing STL models
 stl_folder_path = 'C:/Users/dinob/Desktop/CAD Project/CAD Model Dataset/'
 stl_files = [f for f in os.listdir(stl_folder_path) if f.lower().endswith('.stl')]
 
@@ -59,8 +56,7 @@ else:
 delete_existing_stl_models()
 gc.collect()
 
-# Select the STL file to import
-first_model = stl_files[3]
+first_model = stl_files[4]
 
 # Import the selected STL model
 file_path = os.path.join(stl_folder_path, first_model)
@@ -75,10 +71,9 @@ bpy.context.scene.collection.objects.unlink(imported_object)  # Unlink from the 
 # Scale the object
 bbox = imported_object.dimensions
 largest_dim = max(bbox)
-scale_factor = 0.15 / largest_dim
+scale_factor = 0.27 / largest_dim
 imported_object.scale = (scale_factor, scale_factor, scale_factor)
 
-# Set the object's location to the center of the table (x = 0, y = 0)
 drop_location = (0, 0, table_height + 0.6)
 
 # Assign Rigid Body physics to the object
@@ -90,7 +85,6 @@ imported_object.rigid_body.friction = 0.8
 imported_object.rigid_body.restitution = 0.3 
 imported_object.pass_index = 1
 
-# Set the initial position above the table
 imported_object.location = drop_location
 
 imported_object.rigid_body.collision_shape = 'CONVEX_HULL' 
@@ -103,21 +97,17 @@ bpy.ops.rigidbody.object_add()
 table_object.rigid_body.type = 'PASSIVE'  
 table_object.rigid_body.friction = 1  
 
-# Update the scene
 bpy.context.view_layer.update()
 
-# Apply an existing material to the imported object
 material_name = "resin" 
 
 # Check if the material exists in the Blender environment
 if material_name in bpy.data.materials:
     material = bpy.data.materials[material_name]
     
-    # If the object already has materials, clear them
     if imported_object.data.materials:
         imported_object.data.materials.clear()
     
-    # Assign the existing material to the object
     imported_object.data.materials.append(material)
 else:
     print(f"Material '{material_name}' not found in Blender environment.")
@@ -125,10 +115,7 @@ else:
 # Redefine the origin of the imported object
 redefine_origin(imported_object)
 
-# Set the frame number when the animation should stop
 stop_frame = 25
-
-# List of rotation angles
 z_angles = [0, 45, 90, 135, 180, 225, 270, 315]
 x_angles = [40, 30, 0, -30, -60, -90]
 
@@ -137,11 +124,9 @@ empty_object = bpy.data.objects.get('Empty')
 if empty_object is None:
     raise ValueError("Object 'Empty' not found in the scene.")
 
-# Repeat the process
-for iteration in range(1, 5):
+for iteration in range(1, 7):
     print(f"\n=== Starting iteration {iteration} ===")
 
-    # Reset the object's random position and rotation
     reset_object_and_animation(imported_object, drop_location)
 
     # Progress through the frames to simulate the object's fall
@@ -157,15 +142,11 @@ for iteration in range(1, 5):
             empty_object.rotation_euler[2] = z_angle * (3.14159 / 180) 
             empty_object.rotation_euler[0] = x_angle * (3.14159 / 180) 
 
-            # Update the scene before rendering
             bpy.context.view_layer.update()
-
-            # Set the frame to the last one and render
             bpy.context.scene.frame_set(stop_frame)
 
-            # Specify the output path for each combination of Z and X angle renders
-            main_output_path = f"C:/Users/dinob/Desktop/CAD Project/Prismatic Geometries/null_{iteration}{x_index}{z_index}"
-            mask_output_path = f"null_m{iteration}{x_index}{z_index}"
+            main_output_path = f"C:/Users/dinob/Desktop/CAD Project/Prismatic Geometries/null_{iteration+18}{x_index}{z_index}"
+            mask_output_path = f"null_m{iteration+18}{x_index}{z_index}"
             
             # Set paths for each output node
             node_tree = bpy.context.scene.node_tree
