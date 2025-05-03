a0f6177de6d315b2f6488bd1865f54cf0825e52e
Reinaldo Moraga <naldo.amb@gmail.com>
2025-05-03 12:30:24 -0500

    added documentation

--- Blender Project/mask_percentage.py
+++ Blender Project/mask_percentage.py
@@ -2,11 +2,15 @@ import cv2
 import numpy as np
 import os
 
+''' The purpose of this script is to take an image mask and determine the percentage of foreground in the mask.
+    This helps us determine how much of the occluded object of interest is actually being shown as a percentage,
+    which helps us sort our occluded images and pick between a percentage range'''
+
 def process_masks(input_folder, output_folder, object_name):
     # Ensure output folder exists
     os.makedirs(output_folder, exist_ok=True)
 
-    # Get all image files in the input folder
+    # Get all image files in the masks folder
     image_files = [f for f in os.listdir(input_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     for i, filename in enumerate(image_files):
@@ -18,7 +22,7 @@ def process_masks(input_folder, output_folder, object_name):
         # Convert to binary (thresholding)
         _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 
-        # Count white (mask) and total pixels
+        # Count white (foreground) and total pixels
         white_pixels = np.count_nonzero(binary_image == 255)
         total_pixels = binary_image.size
 
@@ -33,9 +37,9 @@ def process_masks(input_folder, output_folder, object_name):
         cv2.imwrite(new_image_path, binary_image)
         print(f"Processed: {filename} -> {new_filename}")
 
-# Example usage
-input_folder = "/home/rmoraga/CAD Project/Prismatic Geometries/synthetic_dataset/train/masks"  # Change this to your input folder path
-output_folder = "/home/rmoraga/CAD Project/test"  # Change this to your output folder path
+# Define paths
+input_folder = "/home/rmoraga/CAD Project/Prismatic Geometries/synthetic_dataset/train/masks"  # Path to masks folder
+output_folder = "/home/rmoraga/CAD Project/test"
 object_name = "bolt"  # Change this to your object name
 
 process_masks(input_folder, output_folder, object_name)
